8e0d8cf96b33fa1f0b42c114a2e9867dc117c94f
Meggapixxel <vzhydenko@competo.io>
2020-06-14 00:49:17 +0300

    missing file

---
+++ SwipeTabBarController/Controllers/Github/Repositories/GithubRepositoriesVC.swift
@@ -0,0 +1,40 @@
+//
+//  GithubRepositoriesVC.swift
+//  SwipeTabBarController
+//
+//  Created by Vadim Zhydenko on 13.06.2020.
+//  Copyright © 2020 Vadym Zhydenko. All rights reserved.
+//
+
+import UIKit
+
+final class GithubRepositoriesVC: UITableViewController {
+    
+    // MARK: - UI elements
+    
+    // MARK: - Private properties
+    
+    // MARK: - Init
+//    init(
+//        user: GithubUserNO,
+//        githubRepositoriesService: P_GithubRepositoriesService
+//    ) {
+//        self.user = user
+//        self.githubRepositoriesService = githubRepositoriesService
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @available(iOS, unavailable)
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+// MARK: - Setup UI
